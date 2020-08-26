b80c560cb2848791fd762dfe7fb8984d5d46ac49
Siwoo Kim <skim327@myseneca.ca>
2020-08-26 19:21:20 -0400

    KMP

--- src/main/java/baekjoon/p1000/P1701.java
+++ src/main/java/baekjoon/p1000/P1701.java
@@ -3,17 +3,22 @@ package baekjoon.p1000;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class P1305 {
+public class P1701 {
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(reader.readLine());
         String s = reader.readLine();
-        int[] fail = fail(s);
-        System.out.println(N - fail[N-1]);
+        int answer = 0;
+        do {
+            int[] fail = fail(s);
+            answer = Math.max(Arrays.stream(fail).max().getAsInt(), answer);
+            s = s.substring(1);
+        } while (s.length() != 0);
+        System.out.println(answer);
     }
-
+    
     private static int[] fail(String s) {
         int N = s.length();
         int[] fail = new int[N];
@@ -21,10 +26,8 @@ public class P1305 {
         for (int i=1; i<N; i++) {
             while (j > 0 && s.charAt(i) != s.charAt(j))
                 j = fail[j-1];
-            if (s.charAt(i) == s.charAt(j))
+            if (s.charAt(j) == s.charAt(i))
                 fail[i] = ++j;
-            else
-                fail[i] = 0;
         }
         return fail;
     }
